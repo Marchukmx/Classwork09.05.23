74c159bd7779f3740259762c2c36e824fe7aabff
student <vovakond2008@gmail.com>
2023-05-09 18:52:38 +0300

    змінили прінт

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class School:
         self.students = students #Список студентів
      def admit_student(self, student):
         self.students.append(student)
-        print(f'{student.name}') #Дописати, коли створено клас студентів
+        print(f'{student.name} був додани до школи {self.name}') #Дописати, коли створено клас студентів
 class Student:
     def __init__(self,name,grade):
         self.name = name
