ad6538aa29f64d1e52b07cd04889adffdf1bd139
student <vovakond2008@gmail.com>
2023-05-09 18:50:47 +0300

    Класная работа на день победы

---
+++ main.py
@@ -0,0 +1,12 @@
+#Симулятор навчального закладу (база даних)
+class School:
+     def __init__(self, name, students):
+        self.name = name
+        self.students = students #Список студентів
+     def admit_student(self, student):
+        self.students.append(student)
+        print(f'{student.name}') #Дописати, коли створено клас студентів
+class Student:
+    def __init__(self,name,grade):
+        self.name = name
+        self.grade = grade
