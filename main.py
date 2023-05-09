b93074af609e505a11d85b6bb1e292864fd83b68
student <vovakond2008@gmail.com>
2023-05-09 20:01:32 +0300

    Обнова

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ class School:
      def admit_student(self, student):
         self.students.append(student)
         print(f'{student.name} був додани до школи {self.name}') #Дописати, коли створено клас студентів
+     def expel_student(self,student):
+         expelled_student = next(filter(lambda s: s.name == student.name
+                                        and s.grage == student.grade))
+         if expel_student is not None:
+             self.students.remove(expelled_student)
+             print(f"{expelled_student} був видалений з {self.name}")
+         else:
+             print(f"{expelled_student.name} не був видалений {self.name}")
 class Student:
     def __init__(self,name,grade):
         self.name = name
@@ -18,3 +26,20 @@ class Student:
         print(f"{self.name} був понижений до рангу {self.grade}")
     def __str__(self):
         return f"{self.name} - Ранг {self.grade}"
+
+alisa = Student("Alisa",6)
+masha = Student("Masha",2)
+kostya = Student("Kostya",50)
+dima = Student("Dima",23)
+gleb = Student("Gleb",100)
+
+my_school = School("ItStep",[alisa,masha,kostya,dima,gleb])
+
+
+'''
+multiply = lambda x, y: x * y
+print(multiply(2,5))
+numbers = [1,2,3,4,5,6,7,8,9,10]
+filtered_numbers = list(filter(lambda x: x%2 == 0,numbers))
+print(filtered_numbers)
+'''
